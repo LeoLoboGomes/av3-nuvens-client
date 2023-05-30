9b45b7facae402fdd68a2198fed571629eee347e
Leo Gomes <leo.gomes@seplag.ce.gov.br>
2023-05-30 17:04:34 -0300

    Start to make others request functions

--- src/script.js
+++ src/script.js
@@ -16,16 +16,52 @@ const fetchProductById = async (id) => {
   return data[0];
 }
 
-
 //Create new product
 
 
-//Change product
+
+//Edit product
+
+
 
 //Delete product
-console.log('Hello world')
 
-const mockProducts = ["Product one", "Product two", "Product three"]
+
+
+console.log('Hello world');
+
+const insertProduct = async () => {
+  const productName = document.getElementById("insert-name").value;
+  const productPrice = document.getElementById("insert-price").value;
+
+  if (productName && productPrice) {
+    
+  } else {
+    alert("Nome ou preço do produto não foram informados");
+  }
+};
+
+const editProduct = async () => {
+  const productId = document.getElementById("edit-id").value;
+  const productName = document.getElementById("edit-name").value;
+  const productPrice = document.getElementById("edit-price").value;
+
+  if (productId && productName && productPrice) {
+    
+  } else {
+    alert("ID, nome ou preço do produto não foram informados");
+  }
+};
+
+const deleteProduct = async () => {
+  const productId = document.getElementById("delete-id").value;
+
+  if (productId) {
+    
+  } else {
+    alert("ID não foi informado");
+  }
+};
 
 const getProductById = async () => {
   const productId = document.getElementById("product-id").value;
@@ -41,7 +77,7 @@ const getProductById = async () => {
   } else {
     alert("ID do produto não foi informado");
   }
-}
+};
 
 const getAllProducts = async () => {
   console.log('Hello world');
@@ -53,4 +89,4 @@ const getAllProducts = async () => {
   var addHtml = document.createElement('div');
   addHtml.innerHTML = productsList[1].name;
   listParent.appendChild(addHtml);
-}
+};
